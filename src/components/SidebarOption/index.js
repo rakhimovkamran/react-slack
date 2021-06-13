41a9af99bedbede7b116a699a819ab3567fd1fd6
Rakhimov Kamran <rakhimovkamran@gmail.com>
2021-06-14 00:55:52 +0500

    | Store ( Initialize ) | Sidebar | Add ( Enter Room Action )

--- src/components/SidebarOption/index.js
+++ src/components/SidebarOption/index.js
@@ -1,8 +1,12 @@
 import { db } from "constants/firebase.config"
+import { useDispatch } from "react-redux"
+import { enterRoom } from "store/slices/app"
 
 import { S } from "./index.styled"
 
 export const SidebarOption = ({ Icon, title, addChannelOption, id }) => {
+    const dispatch = useDispatch()
+
     const addChannel = () => {
         const channelName = window.prompt("Please enter the channel name")
 
@@ -14,7 +18,13 @@ export const SidebarOption = ({ Icon, title, addChannelOption, id }) => {
     }
 
     const selectChannel = () => {
-        console.log(id)
+        if (id) {
+            dispatch(
+                enterRoom({
+                    roomID: id,
+                })
+            )
+        }
     }
 
     return (
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit"
 
-import counterReducer from "./slices/counter"
+import appReducer from "./slices/app"
 
 export const store = configureStore({
     reducer: {
-        counter: counterReducer,
+        app: appReducer,
     },
 })
