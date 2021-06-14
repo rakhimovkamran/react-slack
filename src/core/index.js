c2aeec1e1547aea45bdc9b7c3fe2a0507ed22a47
Rakhimov Kamran <rakhimovkamran@gmail.com>
2021-06-14 15:22:03 +0500

    | Auth | Finished | Loader | Implemented

--- src/core/index.js
+++ src/core/index.js
@@ -4,20 +4,33 @@ import { S } from "./index.styled"
 import { Header } from "components/Header"
 import { Sidebar } from "components/Sidebar"
 
+import { useAuthState } from "react-firebase-hooks/auth"
+import { auth } from "constants/firebase.config"
+
 import { Chat } from "pages/Chat"
+import { Login } from "pages/Login"
+import { Loading } from "pages/Loading"
 
 export const Core = () => {
+    const [user, isLoading] = useAuthState(auth)
+
     return (
         <>
-            <Header />
-            <S.ApplicationWrapper>
-                <Sidebar />
-                <Switch>
-                    <Route path="/" exact>
-                        <Chat />
-                    </Route>
-                </Switch>
-            </S.ApplicationWrapper>
+            {!user ? (
+                <>{isLoading ? <Loading /> : <Login />}</>
+            ) : (
+                <>
+                    <Header />
+                    <S.ApplicationWrapper>
+                        <Sidebar />
+                        <Switch>
+                            <Route path="/" exact>
+                                <Chat />
+                            </Route>
+                        </Switch>
+                    </S.ApplicationWrapper>
+                </>
+            )}
         </>
     )
 }
